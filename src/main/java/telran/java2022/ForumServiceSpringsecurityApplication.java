c826b2c783d214e031b081e474952cb059caef7c
HeavenAscendedPickle <HeavenAscendedPickle@Asus-Laptop.lan>
2022-11-16 20:50:04 +0200

    first commit

---
+++ src/main/java/telran/java2022/ForumServiceSpringsecurityApplication.java
@@ -0,0 +1,13 @@
+package telran.java2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumServiceSpringsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumServiceSpringsecurityApplication.class, args);
+	}
+
+}
