c7a29f3e2e59e2b27e34bed8558e166e32555ce7
HeavenAscendedPickle <HeavenAscendedPickle@Asus-Laptop.lan>
2022-11-17 16:30:17 +0200

    added authorization

---
+++ src/main/java/telran/java2022/security/service/CustomWebSecurity.java
@@ -0,0 +1,18 @@
+package telran.java2022.security.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import telran.java2022.post.dao.PostRepository;
+import telran.java2022.post.model.Post;
+
+@Service
+@RequiredArgsConstructor
+public class CustomWebSecurity {
+    final PostRepository postRepository;
+
+    public boolean checkPostAuthor(String postId, String userName) {
+	Post post = postRepository.findById(postId).orElse(null);
+	return post != null && userName.equals(post.getAuthor());
+    }
+}
